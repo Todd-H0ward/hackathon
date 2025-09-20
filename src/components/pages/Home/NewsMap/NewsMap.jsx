e0b6ad243dcae682cfba2896ca9eada95e9d2f57
Denis <dr144371@gmail.com>
2025-09-20 15:57:52 +0300

    Add loader

--- src/components/pages/Home/NewsMap/NewsMap.jsx
+++ src/components/pages/Home/NewsMap/NewsMap.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import { Card, Text, Title, Badge, Group } from '@mantine/core';
+import { Card, Text, Title, Badge, Group, Loader } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@/hooks/useStore.js';
 import styles from './NewsMap.module.scss';
 
 const NewsMap = observer(() => {
-  const { places, fetchPlaces } = useStore().newsMap;
+  const { places, fetchPlaces, isLoading } = useStore().newsMap;
 
   useEffect(() => {
     fetchPlaces();
@@ -24,42 +24,51 @@ const NewsMap = observer(() => {
   };
 
   return (
-    <MapContainer
-      center={[56.858745, 35.917421]}
-      zoom={10}
-      className={styles.root}
-    >
-      <TileLayer
-        zIndex={10}
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {places.map((place) => (
-        <Marker
-          key={place.id}
-          position={[place.lat, place.lng]}
-          icon={getCustomIcon(place.type === 'SHELTER' ? 'blue' : 'green')}
+    <>
+      {isLoading ? (
+        <div className={styles.loader}>
+          <Loader size="lg" />
+        </div>
+      ) : (
+        <MapContainer
+          center={[56.858745, 35.917421]}
+          zoom={10}
+          className={styles.root}
         >
-          <Popup>
-            <Card shadow="sm" padding="md">
-              <Title order={3}>{place.name}</Title>
-              <Text size="sm" c="dimmed">
-                <strong>Адрес:</strong> {place.address}
-              </Text>
-              <Text size="sm">
-                <strong>Тип:</strong> <Badge color="blue">{place.type}</Badge>
-              </Text>
-              <Text size="sm">
-                <strong>Вместимость:</strong> {place.capacity} человек
-              </Text>
-              <Text size="sm">
-                <strong>Регион:</strong> {place.regionCode}
-              </Text>
-            </Card>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+          <TileLayer
+            zIndex={10}
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {places.map((place) => (
+            <Marker
+              key={place.id}
+              position={[place.lat, place.lng]}
+              icon={getCustomIcon(place.type === 'SHELTER' ? 'blue' : 'green')}
+            >
+              <Popup>
+                <Card shadow="sm" padding="md">
+                  <Title order={3}>{place.name}</Title>
+                  <Text size="sm" c="dimmed">
+                    <strong>Адрес:</strong> {place.address}
+                  </Text>
+                  <Text size="sm">
+                    <strong>Тип:</strong>{' '}
+                    <Badge color="blue">{place.type}</Badge>
+                  </Text>
+                  <Text size="sm">
+                    <strong>Вместимость:</strong> {place.capacity} человек
+                  </Text>
+                  <Text size="sm">
+                    <strong>Регион:</strong> {place.regionCode}
+                  </Text>
+                </Card>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      )}
+    </>
   );
 });
 
