12806b1534bae415e7320ba00ac0871a953ad9b8
Denis <dr144371@gmail.com>
2025-09-21 09:21:03 +0300

    Remove account popover

--- src/components/commons/Header/Header.jsx
+++ src/components/commons/Header/Header.jsx
@@ -1,15 +1,12 @@
-import { AppShell, Button, Group, Text } from '@mantine/core';
+import { AppShell, Group, Text } from '@mantine/core';
 import { ShieldCheck } from 'lucide-react';
-import AccountDropdown from './AccountDropdown/index.js';
 import { STATIC_LINKS } from '@/constants/staticLinks.js';
 import { Link, useLocation } from 'react-router';
 import { observer } from 'mobx-react-lite';
-import { useStore } from '@/hooks/useStore.js';
 
 import styles from './Header.module.scss';
 
 const Header = observer(() => {
-  const { isAuth, account, logout } = useStore().auth;
   const { pathname } = useLocation();
 
   if (pathname === STATIC_LINKS.LOGIN || pathname === STATIC_LINKS.REGISTER) {
@@ -22,17 +19,11 @@ const Header = observer(() => {
         <Link className={styles.link} to={STATIC_LINKS.HOME}>
           <Group gap="xs" align="center">
             <ShieldCheck size={34} />
-            <Text fw={600} size="lg">Идентификатор происшествий</Text>
+            <Text fw={600} size="lg">
+              Идентификатор происшествий
+            </Text>
           </Group>
         </Link>
-
-        {isAuth ? (
-          <AccountDropdown account={account} onLogout={logout} />
-        ) : (
-          <Link to={STATIC_LINKS.LOGIN}>
-            <Button>Войти</Button>
-          </Link>
-        )}
       </Group>
     </AppShell.Header>
   );
